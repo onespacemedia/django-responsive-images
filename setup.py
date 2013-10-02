929aa997e2febafec68bb81162edf460673ffe15
Daniel Samuels <daniel.samuels1@gmail.com>
2013-10-02 10:16:10 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='Django Responsive Images',
+    version='1.0',
+    description='Provides responsive image generation in Django.',
+    author='Daniel Samuels',
+    author_email='daniel@onespacemedia.com',
+    url='https://github.com/onespacemedia/django-responsive-images/',
+    packages=['responsive_images'],
+)
